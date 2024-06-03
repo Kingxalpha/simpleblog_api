c5f030088ee857906c7f0540391068afeb99ec59
kingxalpha <tosinsirmuel@gmail.com>
2024-06-03 22:02:29 +0100

    password

--- routes/auth.js
+++ routes/auth.js
@@ -59,5 +59,33 @@ router.get('/profile', auth, async (req, res) => {
         res.status(500).send('Server error');
     }
 });
+// Update password
+router.patch('/password/:id', async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+  
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ msg: 'User not found' });
+      }
+  
+      
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Incorrect current password' });
+      }
+  
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+  
+      await user.save();
+  
+      res.json({ msg: 'Password updated successfully' });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  });
+  
 
 module.exports = router;
